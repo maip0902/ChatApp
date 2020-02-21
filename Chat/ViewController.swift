a5af1b61d628031ce7a258c2e001a51b0f189a96
Mai Kanda <kanda@arsaga.jp>
2020-02-21 13:08:11 +0900

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by 舞 on 2020/02/21.
+//  Copyright © 2020 mai kanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
