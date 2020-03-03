1a84df01df4fd8f609460748882a725c838ae1c3
Mai Kanda <kanda@arsaga.jp>
2020-03-03 16:36:19 +0900

    first commit

---
+++ Chat/MockMessage.swift
@@ -0,0 +1,13 @@
+//
+//  MockMessage.swift
+//  Chat
+//
+//  Created by 舞 on 2020/02/25.
+//  Copyright © 2020 mai kanda. All rights reserved.
+//
+
+import UIKit
+
+class MockMessage: NSObject {
+
+}
